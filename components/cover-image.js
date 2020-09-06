30d66bc3d94352376263ce83777d49fd63c799f8
Mauricio Peraza <mauricio.peraza@revinate.com>
2020-09-05 18:18:01 -0600

    initial changes to make it work

--- components/cover-image.js
+++ components/cover-image.js
@@ -6,7 +6,7 @@ export default function CoverImage({ title, url, slug }) {
     <img
       src={url}
       alt={`Cover Image for ${title}`}
-      className={cn('shadow-small', {
+      className={cn('shadow-small mx-auto', {
         'hover:shadow-medium transition-shadow duration-200': slug,
       })}
     />
