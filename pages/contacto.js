8cfd1a9f387edff16102aa003478f407c73d55ff
Mauricio Peraza <mauricio.peraza@revinate.com>
2020-09-18 17:40:54 -0600

    Contact page and Author page work

--- pages/contacto.js
+++ pages/contacto.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Container from '../components/container';
 import Layout from '../components/layout';
+import Header from '../components/header';
 import { BLOG_NAME } from '../lib/constants';
 
 function Contact({ preview }) {
@@ -11,7 +12,40 @@ function Contact({ preview }) {
           <title>{BLOG_NAME} - Artículos de teología y más</title>
         </Head>
         <Container>
-          <p>Future CONTACT PAGE</p>
+          <section className='body-font relative'>
+            <div className='container px-5 mx-auto'>
+              <Header title='Contáctenos' />
+              <div className='lg:w-1/2 md:w-2/3 mx-auto'>
+                <div className='flex flex-wrap -m-2'>
+                  <div className='p-2 w-1/2'>
+                    <input
+                      className='w-full bg-gray-100 rounded border border-gray-400 focus:outline-none focus:border-blue-500 text-base px-4 py-2'
+                      placeholder='Nombre'
+                      type='text'
+                    />
+                  </div>
+                  <div className='p-2 w-1/2'>
+                    <input
+                      className='w-full bg-gray-100 rounded border border-gray-400 focus:outline-none focus:border-blue-500 text-base px-4 py-2'
+                      placeholder='Email'
+                      type='email'
+                    />
+                  </div>
+                  <div className='p-2 w-full'>
+                    <textarea
+                      className='w-full bg-gray-100 rounded border border-gray-400 focus:outline-none h-48 focus:border-blue-500 text-base px-4 py-2 resize-none block'
+                      placeholder='Mensaje'
+                    ></textarea>
+                  </div>
+                  <div className='p-2 w-full'>
+                    <button className='flex mx-auto text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded text-lg'>
+                      Enviar
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
         </Container>
       </Layout>
     </>
--- pages/sobre-juan-stam.js
+++ pages/sobre-juan-stam.js
@@ -1,10 +1,10 @@
 import Head from 'next/head';
 import Container from '../components/container';
-import Header from '../components/header';
-import Intro from '../components/intro';
+import markdownStyles from '../components/markdown-styles.module.css';
 import Layout from '../components/layout';
 import { getAuthor } from '../lib/api';
 import { BLOG_NAME } from '../lib/constants';
+import ReactMarkdown from 'react-markdown';
 
 function Autor(props) {
   return (
@@ -15,9 +15,44 @@ function Autor(props) {
             <title>{BLOG_NAME} - Artículos de teología y más</title>
             <meta property='og:image' content={props.picture} />
           </Head>
-          <Intro />
-          <h1>{props.name}</h1>
-          <p>{props.biography}</p>
+          <section className='body-font'>
+            <div className='container py-12 mx-auto flex flex-col'>
+              <div className='lg:w-4/6 mx-auto'>
+                <div className='rounded-lg h-full overflow-hidden'>
+                  <img
+                    alt='content'
+                    className='object-cover object-center h-full w-full'
+                    src={props.picture2.url}
+                  />
+                </div>
+                <div className='flex flex-col sm:flex-row mt-10'>
+                  <div className='sm:w-1/3 text-center sm:pr-8 sm:py-8'>
+                    <div className='inline-flex items-center justify-center text-gray-400'>
+                      <img
+                        className='w-20 h-20 rounded-full object-cover'
+                        src={props.picture.url}
+                        alt={props.picture.name}
+                      />
+                    </div>
+                    <div className='flex flex-col items-center text-center justify-center'>
+                      <h2 className='font-medium title-font mt-4 text-gray-900 text-lg'>
+                        {props.name}
+                      </h2>
+                      <div className='w-12 h-1 bg-blue-500 rounded mt-2 mb-4'></div>
+                      <p className='text-base text-gray-600'>
+                        <ReactMarkdown source={props.excerpt} />
+                      </p>
+                    </div>
+                  </div>
+                  <div className='sm:w-2/3 sm:pl-8 sm:py-8 sm:border-l border-gray-300 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-center sm:text-left'>
+                    <div className={markdownStyles['markdown']}>
+                      <ReactMarkdown source={props.biography} />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
         </article>
       </Container>
     </Layout>
