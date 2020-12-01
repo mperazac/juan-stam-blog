0a0af1a028a1f43955863c0b99a1d2b3d626f606
Mauricio Peraza <mauricio.peraza@revinate.com>
2020-12-01 13:48:15 -0600

    Fix error while removing app.css stylesheet

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import { useEffect } from 'react'
 import { useRouter } from 'next/router'
 import * as gtag from '../lib/gtag'
+import '../styles/index.css';
 
 const App = ({ Component, pageProps }) => {
   const router = useRouter()
